b242f29f228fb51eb831e0604f7f70baf57f862c
DoolyDoori <leedoori11011@gmail.com>
2023-06-11 22:36:53 +0900

    [ADD] 센터정보에 지도위치 수정, 마커추가

--- pages/posts/centerInfo.tsx
+++ pages/posts/centerInfo.tsx
@@ -1,19 +1,20 @@
-import Head from 'next/head'
-import clientPromise from '../../lib/mongodb'
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
-import React from 'react'
-import {useRouter} from 'next/router'
-import { Map, MapMarker,  } from "react-kakao-maps-sdk"
+import Head from 'next/head';
+import clientPromise from '../../lib/mongodb';
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
+import { Map, MapMarker, useMap } from 'react-kakao-maps-sdk';
+import { useState } from 'react';
+
 
 type ConnectionStatus = {
-  isConnected: boolean
-}
+  isConnected: boolean;
+};
 
 export const getServerSideProps: GetServerSideProps<
   ConnectionStatus
 > = async () => {
   try {
-    await clientPromise
+    await clientPromise;
     // `await clientPromise` will use the default database passed in the MONGODB_URI
     // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
     //
@@ -25,20 +26,51 @@ export const getServerSideProps: GetServerSideProps<
 
     return {
       props: { isConnected: true },
-    }
+    };
   } catch (e) {
-    console.error(e)
+    console.error(e);
     return {
       props: { isConnected: false },
-    }
+    };
   }
-}
+};
 
 export default function Home({
   isConnected,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const router = useRouter();
 
-    const router = useRouter();
+const mapData = [
+  {
+    content: <div style={{ color: '#000' }}> Expeto </div>,
+    latlng: { lat: 37.56677916106135, lng: 126.97869754787648 },
+  },
+  {
+    content: <div style={{ color: '#000' }}> 서울종로경찰서 </div>,
+    latlng: { lat: 37.57180757275303, lng: 126.97869754787648 },
+  },
+  {
+    content: <div style={{ color: '#000' }}> 서울서대문경찰서 </div>,
+    latlng: { lat: 37.56489333530958, lng: 126.9668077228793 },
+  },
+];
+
+  const EventMarkerContainer = ({position, content}) => {
+    const map = useMap();
+    const [isVisible, setIsVisible] = useState(false);
+
+    return (
+      <MapMarker
+        position={position} // 마커를 표시할 위치
+        // @ts-ignore
+        onClick={(marker) => map.panTo(marker.getPosition())}
+        onMouseOver={() => setIsVisible(true)}
+        onMouseOut={() => setIsVisible(false)}
+      >
+        {isVisible && content}
+      </MapMarker>
+    );
+  };
 
   return (
     <div className="container">
@@ -49,31 +81,46 @@ export default function Home({
       <header className="Header"></header>
       <div className="logo">
         <p>분실물 센터</p>
-        <h3 className="logo1">주소: 중부대학교</h3>
-        <h3 className="logo2">연락처: 02-XXX-XXXX</h3>
+        <h3 className="logo1">익스펙토</h3>
+        <h3 className="logo2">연락처: 02-123-4567</h3>
       </div>
-      <div>
+
+      <div className="map">
         <Map
-          center={{ lat: 33.5563, lng: 126.79581 }}
-          style={{ width: '100%', height: '360px' }}
+          center={{
+            lat: 37.56677916106135,
+            lng: 126.97869754787648,
+          }}
+          style={{
+            width: '400px',
+            height: '400px',
+          }}
+          level={6} // 지도의 확대 레벨
         >
-          <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-            <div style={{ color: '#000' }}>Hello World!</div>
-          </MapMarker>
+          {mapData.map((value) => (
+            <EventMarkerContainer
+              key={`EventMarkerContainer-${value.latlng.lat}-${value.latlng.lng}`}
+              position={value.latlng}
+              content={value.content}
+            />
+          ))}
         </Map>
       </div>
-      <input
-        className="Button"
-        type="button"
-        value="홈으로"
-        onClick={() => router.push('/posts/QRcheck')}
-      ></input>
+      <div className="btn">
+        <input
+          className="Button"
+          type="button"
+          value="홈으로"
+          onClick={() => router.push('/posts/QRcheck')}
+        ></input>
+      </div>
 
       <style jsx>{`
-        .container1 {
-          margin: 0 auto; /* 화면 중앙에 배치 */
-          width: 1200px; /* 너비 */
-          background-color: #fff;
+        .map {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        
         }
         .Header {
           background-color: blue;
@@ -104,9 +151,12 @@ export default function Home({
           border-color: cornflowerblue;
           border-radius: 15px;
           color: white;
-          margin-top: 40px;
-          margin-bottom: 50px;
-          margin-left: 720px;
+        }
+        .btn {
+          margin: auto 0; /* 화면 중앙에 배치 */
+          position: absolute;
+          left: 50%;
+          transform: translateX(-50%);
         }
       `}</style>
 
