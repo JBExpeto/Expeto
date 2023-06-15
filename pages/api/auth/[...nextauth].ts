98ac4a98a8b50477ba6a20d982e151dd8a00bd95
cya98 <todaudcksgk@naver.com>
2023-06-15 17:51:04 +0900

    [ADD]-로그인오류 수정

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import type { NextAuthOptions } from 'next-auth'
+import { ObjectId } from 'mongodb'
+import clientPromise from '../../../lib/mongodb'
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -11,18 +13,32 @@ export const authOptions: NextAuthOptions = {
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' }
+        // Connect to MongoDB
+        const client = await clientPromise
+        const db = client.db('Expeto')
+        const collection = db.collection('users')
 
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null
+        if (credentials) {
+          // Find user by username and password
+          const user = await collection.findOne({
+            username: credentials.username,
+            password: credentials.password,
+          })
 
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+          if (user) {
+            // Convert MongoDB document to user object
+            const convertedUser = {
+              id: user._id.toHexString(),
+              name: user.name,
+              email: user.email,
+              // Add other user properties as needed
+            }
+
+            return convertedUser
+          }
         }
+
+        return null
       },
     }),
   ],
